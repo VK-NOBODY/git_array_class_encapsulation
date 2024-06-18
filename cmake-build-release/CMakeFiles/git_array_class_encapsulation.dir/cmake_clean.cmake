c29d4ea67fa107d0135e0133d27e9c4d2f04ac41
VK-NOBODY <171213515+VK-NOBODY@users.noreply.github.com>
2024-06-18 15:43:03 +0800

    Initial commit

---
+++ cmake-build-release/CMakeFiles/git_array_class_encapsulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_array_class_encapsulation.dir/main.cpp.obj"
+  "CMakeFiles/git_array_class_encapsulation.dir/main.cpp.obj.d"
+  "git_array_class_encapsulation.exe"
+  "git_array_class_encapsulation.exe.manifest"
+  "git_array_class_encapsulation.pdb"
+  "libgit_array_class_encapsulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_array_class_encapsulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/git_array_class_encapsulation.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for git_array_class_encapsulation.
+# This may be replaced when dependencies are built.
